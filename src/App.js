8568b193c1e09636b67b1d247aebdfb6388c465c
Will Carter <will.carter@fergusllc.com>
2021-01-19 16:47:00 -0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom"
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PortfolioContainer from '../src/containers/PortfolioContainer'
+import ContactContainer from '../src/containers/ContactContainer'
 
 const App = () => {
 
   return (
-    <>
-      <Header />      
-      <PortfolioContainer />
+    <Router>
+      <Header /> 
+      <Switch>
+        <Route exact path="/" component={ PortfolioContainer } />
+        <Route exact path="/page2" component={ PortfolioContainer } />
+        <Route exact path="/about" component={ ContactContainer } />
+      </Switch>
       <Footer />
-    </>
-    
-              )
+    </Router>
+  )
 }
 
 export default App
