c4a1fa1e49fd75da7404add87b3f75676a5eccea
Will Carter <will.carter@fergusllc.com>
2021-01-19 15:58:18 -0700

    add

---
+++ src/components/Pager.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Pager = () => {
+  return (
+    <ul className="pagination">
+      <li className="page-item">
+        <a className="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span> <span className="sr-only">Prev</span>
+        </a>
+      </li>
+      <li className="page-item">
+        <a className="page-link" href="index.html">1</a>
+      </li>
+      <li className="page-item">
+        <a className="page-link" href="page2.html">2</a>
+      </li>
+      <li className="page-item">
+        <a className="page-link" href="page2.html" aria-label="Next">
+          <span className="sr-only">Next</span> <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  )
+}
+
+export default Pager
