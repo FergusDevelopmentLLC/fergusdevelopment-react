aecf42c5e740daddd766a46de9e3e2bc698e0ead
Will Carter <will.carter@fergusllc.com>
2021-01-19 16:15:54 -0700

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+  <footer class="footer">&copy; {new Date().getFullYear() } Fergus Development LLC</footer>
+  )
+}
+
+export default Footer
