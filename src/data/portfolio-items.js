fb7ca8bc1844116352cf2ba9b557b17d00349458
Will Carter <will.carter@fergusllc.com>
2021-01-19 15:58:47 -0700

    add items

--- src/data/portfolio-items.js
+++ src/data/portfolio-items.js
@@ -31,6 +31,7 @@ export const portfolioItems = [
   {
     title: 'USFS-DST proof of concept',
     description: 'A proof of concept front end focused on Rogue River watershed conservation efforts. This application is the focus of my current work with <a href="https://www.thefreshwatertrust.org/">The Freshwater Trust</a>.',      url: 'https://www.politipoint.org/',
+    url: 'http://138.68.23.63:5000/',
     imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091198/portfolio/dst_800x400_bo8y5o.png',
     btnText: 'Demo',
     links : [
@@ -58,6 +59,83 @@ export const portfolioItems = [
         prefix: 'Blog post: ',
         url: 'https://www.thefreshwatertrust.org/data-visualizing-a-better-future-for-the-rogue/',
         linkText: 'Data Visualizing a Better Future for the Rogue'
+      }
+    ]
+  },
+  {
+    title: 'Hikefinder.net',
+    description: 'Hikefinder.net is a resource to find hiking trails across the United States. Contains over 30,000 hiking trails, searchable by city/town or by pinpointing a location on the map.',
+    url: 'https://www.hikefinder.net/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091204/portfolio/hikefinder_02_wmjmxb.png',
+    btnText: 'Find a trail near you!',
+    links : [
+      {
+        prefix: 'Back end: Ruby on Rails API - ',
+        url: 'https://github.com/FergusDevelopmentLLC/hiking_project_api',
+        linkText: 'Github repository'
+      },
+      {
+        prefix: 'Front end - Vanilla JS - ',
+        url: 'https://github.com/FergusDevelopmentLLC/hiking_project_fe',
+        linkText: 'Github repository'
+      },
+      {
+        prefix: 'Featured library: ',
+        url: 'https://www.mapbox.com/',
+        linkText: 'Mapbox'
+      },
+      {
+        prefix: 'Blog post: ',
+        url: 'https://will-carter.medium.com/reviving-old-ideas-2f2908189a34',
+        linkText: 'Reviving old ideas'
+      }
+    ]
+  },
+  {
+    title: 'Skillcraft',
+    description: 'Skillcraft is an online classroom manager inspired by Google Classroom, written in Ruby on Rails.',
+    url: 'http://skillcraft.herokuapp.com/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091197/portfolio/skillcraft_800x400_revhug.png',
+    btnText: 'Demo',
+    links : [
+      {
+        prefix: 'Ruby on Rails Front/Back end - ',
+        url: 'https://github.com/FergusDevelopmentLLC/skillcraft',
+        linkText: 'Github repository'
+      },
+      {
+        prefix: 'Blog post: ',
+        url: 'https://will-carter.medium.com/learning-by-4f5d58192fe6',
+        linkText: 'SkillCrafting: Building an Online Learning Platform with Ruby on Rails'
+      }
+    ]
+  },
+  {
+    title: 'Popular U.S. Names of the Last Century (1910-2016)',
+    description: 'This D3.js based map shows occurrences by state over time for a particular name. User interfaces allow for name lookup and a slider shows how the name evolves over time. Based on the top 1000 names for each year. Over 33,000 unique and dynamic names/maps possible.',
+    url: 'http://104.236.16.91:8645/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091201/portfolio/names_800x400_v2_dtxvdm.png',
+    btnText: 'Map',
+    links : [
+      {
+        prefix: 'Back end: ',
+        url: 'http://postgis.net/',
+        linkText: 'PostGreSQL/PostGIS'
+      },
+      {
+        prefix: 'Front end - Vanilla JS - ',
+        url: 'https://github.com/FergusDevelopmentLLC/names-map-node',
+        linkText: 'Github repository'
+      },
+      {
+        prefix: 'Featured library: ',
+        url: 'https://nodejs.org',
+        linkText: 'Node.js'
+      },
+      {
+        prefix: 'Featured library: ',
+        url: 'https://www.mapbox.com/',
+        linkText: 'D3.js'
       }
     ]
   },
