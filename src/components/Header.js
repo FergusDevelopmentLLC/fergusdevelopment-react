f060e24b1de53e7ee5b548a2afb37df8242de344
Will Carter <will.carter@fergusllc.com>
2021-01-19 16:47:14 -0700

    make about link dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import {  } from 'react-router-dom'
 
 const Header = () => {
+  
+  const location = useLocation()
+  
   return (
-    <header class="header">
-      <div class="header-content">
-        <div class="nav-wrapper">
-          <div class="nav-title">Will Carter: Full Stack Web Developer</div>
-          <div class="nav">
+    <header className="header">
+      <div className="header-content">
+        <div className="nav-wrapper">
+          <div className="nav-title">Will Carter: Full Stack Web Developer</div>
+          <div className="nav">
             <ul>
-              <li><a href="contact.html">Contact</a></li>
+              <li><Link to={ location.pathname === '/about' ? '/' : '/about' }>{ location.pathname === '/about' ? 'Portfolio' : 'About' }</Link></li>
             </ul>
           </div>
         </div>
