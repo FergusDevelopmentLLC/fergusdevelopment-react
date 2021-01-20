a7bc9fce0da7ee54a1f7065af7c2d0350355049b
Will Carter <will.carter@fergusllc.com>
2021-01-19 20:12:55 -0700

    fixed classname bug

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -7,7 +7,7 @@ const Pager = () => {
 
   return (
     <ul className="pagination">
-      <li className="page-item" className={ location.pathname === '/' ? 'hidden' : '' }>
+      <li className={ location.pathname === '/' ? 'page-item hidden' : 'page-item' }>
         <Link className="page-link" aria-label="Previous" to="/page2"><span aria-hidden="true">&laquo;</span> <span className="sr-only">Prev</span></Link>
       </li>
       <li className="page-item">
@@ -16,7 +16,7 @@ const Pager = () => {
       <li className="page-item">
         <Link className="page-link" to="/page2">2</Link>
       </li>
-      <li className="page-item" className={ location.pathname === '/page2' ? 'hidden' : '' }>
+      <li className={ location.pathname === '/page2' ? 'page-item hidden' : 'page-item' }>
         <Link className="page-link" aria-label="Next" to="/page2"><span className="sr-only">Next</span> <span aria-hidden="true">&raquo;</span></Link>
       </li>
     </ul>
