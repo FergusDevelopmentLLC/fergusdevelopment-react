010d1edad20fb3a642f774dcd82221a822f7fed5
Will Carter <will.carter@fergusllc.com>
2021-01-19 17:27:59 -0700

    add ScrollToTopOnMount

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PortfolioItem from './PortfolioItem'
-import { portfolioItems }  from '../data/portfolio-items'
+import { portfolioItems01 } from '../data/portfolio-items-01'
+import { portfolioItems02 } from '../data/portfolio-items-02'
+import { useLocation } from 'react-router-dom'
 
 const Portfolio = () => {
 
-  return (
-    portfolioItems.map((item) => {
-      return <PortfolioItem portfolioItem = { item } />
-    })
+  const location = useLocation()
+  const portfolioItems = location.pathname === '/' ? portfolioItems01 : portfolioItems02
 
+  //https://reactrouter.com/web/guides/scroll-restoration
+  const ScrollToTopOnMount = () => {
+    useEffect(() => {
+      window.scrollTo(0, 0)
+    }, [])
+  
+    return null
+  }
+
+  const getPortfolioItems = () => {
+    return portfolioItems.map((item, i) => {
+      return <PortfolioItem key={i} portfolioItem = { item } />
+    })
+  }
+
+  return (
+    <>
+    <ScrollToTopOnMount />
+    { getPortfolioItems() }
+    </>
   )
 }
 
