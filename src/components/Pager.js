ca7ce2997f0437cb4657fc3eab252e4a07086d60
Will Carter <will.carter@fergusllc.com>
2021-01-19 17:27:42 -0700

    use Link and make aware of location path

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,25 +1,23 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
-const Pager = ({
-  page = 1
-}) => {
+const Pager = () => {
+
+  const location = useLocation()
+
   return (
     <ul className="pagination">
-      <li className="page-item" className={ page === 1 ? "hidden" : "" }>
-        <a className="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span> <span className="sr-only">Prev</span>
-        </a>
+      <li className="page-item" className={ location.pathname === '/' ? 'hidden' : '' }>
+        <Link className="page-link" aria-label="Previous" to="/page2"><span aria-hidden="true">&laquo;</span> <span className="sr-only">Prev</span></Link>
       </li>
       <li className="page-item">
-        <a className="page-link" href="index.html">1</a>
+        <Link className="page-link" to="/">1</Link>
       </li>
       <li className="page-item">
-        <a className="page-link" href="page2.html">2</a>
+        <Link className="page-link" to="/page2">2</Link>
       </li>
-      <li className="page-item" className={ page === 2 ? "hidden" : "" }>
-        <a className="page-link" href="page2.html" aria-label="Next">
-          <span className="sr-only">Next</span> <span aria-hidden="true">&raquo;</span>
-        </a>
+      <li className="page-item" className={ location.pathname === '/page2' ? 'hidden' : '' }>
+        <Link className="page-link" aria-label="Next" to="/page2"><span className="sr-only">Next</span> <span aria-hidden="true">&raquo;</span></Link>
       </li>
     </ul>
   )
