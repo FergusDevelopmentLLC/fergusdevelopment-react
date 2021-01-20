f720f7caf22b603e971b4d23796218ea055e0dc0
Will Carter <will.carter@fergusllc.com>
2021-01-19 20:13:06 -0700

    added items

--- src/data/portfolio-items-02.js
+++ src/data/portfolio-items-02.js
@@ -1,39 +1,29 @@
 export const portfolioItems02 = [
   {
-    title: 'Page 2',
-    description: 'PolitiPoint.org is a survey that provides insight into political leanings beyond the traditional "right" and "left" framework.',
-    url: 'https://www.politipoint.org/',
-    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091201/portfolio/politipoint_egxibf.gif',
-    btnText: 'Take the survey!',
+    title: 'Games Guide',
+    description: 'GamesGuide is a demonstration Ruby/Sinatra web application created to keep track of a board game collection. Users can login, add games, comment on games and tag them.',
+    url: 'https://gamesguide.herokuapp.com/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091204/portfolio/games_guide_800x400_wcrgcq.png',
+    btnText: 'Demo',
     links : [
       {
-        prefix: 'Back end: Ruby on Rails API - ',
-        url: 'https://github.com/FergusDevelopmentLLC/political_fingerprint_api',
+        prefix: 'Sinatra Front and Back end - ',
+        url: 'https://github.com/FergusDevelopmentLLC/GameGuide',
         linkText: 'Github repository'
-      },
-      {
-        prefix: 'Front end: React/Redux - ',
-        url: 'https://github.com/FergusDevelopmentLLC/politipoint_fe',
-        linkText: 'Github repository'
-      },
-      {
-        prefix: 'Featured library: ',
-        url: 'https://www.mapbox.com/',
-        linkText: 'Mapbox'
       },
       {
         prefix: 'Blog post: ',
-        url: 'https://will-carter.medium.com/reacting-redoing-learning-a643cf0a9966',
-        linkText: 'Rails API to React/Redux with Hooks'
+        url: 'https://will-carter.medium.com/building-a-games-guide-5c1791524ca5',
+        linkText: 'Building a Games Guide'
       }
     ]
   },
   {
-    title: 'USFS-DST proof of concept',
-    description: 'A proof of concept front end focused on Rogue River watershed conservation efforts. This application is the focus of my current work with <a href="https://www.thefreshwatertrust.org/">The Freshwater Trust</a>.',      url: 'https://www.politipoint.org/',
-    url: 'http://138.68.23.63:5000/',
-    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091198/portfolio/dst_800x400_bo8y5o.png',
-    btnText: 'Demo',
+    title: 'Starbucks Location Density: 1991-2017',
+    description: 'This Leaflet map uses hexbinning to convey Starbucks location density. Hexbins are redrawn to make them appropriate to zoom level.',
+    url: 'http://104.236.16.91:8660/starbucks/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091197/portfolio/starbucks_800x400_obkv6p.png',
+    btnText: 'Map',
     links : [
       {
         prefix: 'Back end: ',
@@ -41,97 +31,82 @@ export const portfolioItems02 = [
         linkText: 'SQL/PostGIS'
       },
       {
-        prefix: 'Front end: ',
-        url: 'https://reactjs.org/',
-        linkText: 'React'
-      },
-      {
-        prefix: 'Featured library: ',
-        url: 'https://www.mapbox.com/',
-        linkText: 'Mapbox'
-      },
-      {
-        prefix: 'Featured library: ',
-        url: 'https://d3js.org/',
-        linkText: 'D3.js'
-      },
-      {
-        prefix: 'Blog post: ',
-        url: 'https://www.thefreshwatertrust.org/data-visualizing-a-better-future-for-the-rogue/',
-        linkText: 'Data Visualizing a Better Future for the Rogue'
-      }
-    ]
-  },
-  {
-    title: 'Hikefinder.net',
-    description: 'Hikefinder.net is a resource to find hiking trails across the United States. Contains over 30,000 hiking trails, searchable by city/town or by pinpointing a location on the map.',
-    url: 'https://www.hikefinder.net/',
-    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091204/portfolio/hikefinder_02_wmjmxb.png',
-    btnText: 'Find a trail near you!',
-    links : [
-      {
-        prefix: 'Back end: Ruby on Rails API - ',
-        url: 'https://github.com/FergusDevelopmentLLC/hiking_project_api',
-        linkText: 'Github repository'
-      },
-      {
         prefix: 'Front end - Vanilla JS - ',
-        url: 'https://github.com/FergusDevelopmentLLC/hiking_project_fe',
+        url: 'https://github.com/FergusDevelopmentLLC/starbucks',
         linkText: 'Github repository'
       },
       {
-        prefix: 'Featured library: ',
-        url: 'https://www.mapbox.com/',
-        linkText: 'Mapbox'
-      },
-      {
-        prefix: 'Blog post: ',
-        url: 'https://will-carter.medium.com/reviving-old-ideas-2f2908189a34',
-        linkText: 'Reviving old ideas'
+        prefix: 'Featured map library: ',
+        url: 'https://leafletjs.com/',
+        linkText: 'Leaflet.js'
       }
     ]
   },
   {
-    title: 'Skillcraft',
-    description: 'Skillcraft is an online classroom manager inspired by Google Classroom, written in Ruby on Rails.',
-    url: 'http://skillcraft.herokuapp.com/',
-    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091197/portfolio/skillcraft_800x400_revhug.png',
-    btnText: 'Demo',
-    links : [
-      {
-        prefix: 'Ruby on Rails Front and Back end - ',
-        url: 'https://github.com/FergusDevelopmentLLC/skillcraft',
-        linkText: 'Github repository'
-      },
-      {
-        prefix: 'Blog post: ',
-        url: 'https://will-carter.medium.com/learning-by-4f5d58192fe6',
-        linkText: 'SkillCrafting: Building an Online Learning Platform with Ruby on Rails'
-      }
-    ]
-  },
-  {
-    title: 'Popular U.S. Names of the Last Century (1910-2016)',
-    description: 'This D3.js based map shows occurrences by state over time for a particular name. User interfaces allow for name lookup and a slider shows how the name evolves over time. Based on the top 1000 names for each year. Over 33,000 unique and dynamic names/maps possible.',
-    url: 'http://104.236.16.91:8645/',
-    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091201/portfolio/names_800x400_v2_dtxvdm.png',
+    title: 'Top Boy/Girl names of the Last Century (1910-2016)',
+    description: 'This D3.js based map shows a different perspective on the names data. The map shows top Boy/Girl name occurrences for each year by state. A Python script processed the raw data from the Social Security Administration.',
+    url: 'http://104.236.16.91:8645/top-names',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091196/portfolio/top_names_800x400_shyhjy.png',
     btnText: 'Map',
     links : [
       {
         prefix: 'Back end: ',
         url: 'http://postgis.net/',
-        linkText: 'PostGreSQL/PostGIS'
+        linkText: 'SQL/PostGIS'
       },
       {
         prefix: 'Front end - Vanilla JS - ',
-        url: 'https://github.com/FergusDevelopmentLLC/names-map-node',
+        url: 'https://github.com/FergusDevelopmentLLC/top-names-map',
         linkText: 'Github repository'
       },
       {
         prefix: 'Featured library: ',
-        url: 'https://nodejs.org',
-        linkText: 'Node.js'
+        url: 'https://d3js.com/',
+        linkText: 'd3.js'
+      }
+    ]
+  },
+  {
+    title: 'U.S. Federal Lands (GeoDev Web Stack)',
+    description: 'This project involved building a full web stack from database to front end. Static geojson, PostGRES, and PostGIS queries are compared to deliver large geospatial datasets.',
+    url: 'http://104.236.16.91:8641/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091196/portfolio/fedlands_700x300_mehqkm.png',
+    btnText: 'Map',
+    links : [
+      {
+        prefix: 'Back end: ',
+        url: 'http://postgis.net/',
+        linkText: 'SQL/PostGIS'
       },
+      {
+        prefix: 'Featured library: ',
+        url: 'https://vuejs.org/',
+        linkText: 'Vue.js'
+      },
+      {
+        prefix: 'Featured library: ',
+        url: 'http://knexjs.org/',
+        linkText: 'Knex.js'
+      },
+      {
+        prefix: 'Featured library: ',
+        url: 'https://github.com/hapijs/joi',
+        linkText: 'Joi.js'
+      },
+      {
+        prefix: 'Featured map library: ',
+        url: 'https://leafletjs.com/',
+        linkText: 'Leaflet.js'
+      }
+    ]
+  },
+  {
+    title: 'U.S. 2017 Weekly Drought Conditions',
+    description: 'This D3.js based map shows drought conditions in the U.S. by week during 2017. The speed of D3.js can seen by cycling through the geospatial data for each week.',
+    url: 'http://104.236.16.91:8650/us-weekly-drought-conditions/',
+    imageUrl: 'https://res.cloudinary.com/fergusdev/image/upload/v1611091196/portfolio/drought_v6_eyyldb.gif',
+    btnText: 'Map',
+    links : [
       {
         prefix: 'Featured library: ',
         url: 'https://www.mapbox.com/',
